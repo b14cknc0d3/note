a7c4c771a385706558d569d4e40214630ca0c962
akmakmpp <nctbd5@gmail.com>
2021-11-16 12:53:21 +0630

    update

--- lib/controller/note_controller.dart
+++ lib/controller/note_controller.dart
@@ -2,18 +2,28 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:note/database/database.dart';
 import 'package:note/model/note.dart';
 
 class NoteController extends GetxController {
   RxList<Note> notes = <Note>[].obs;
   final TextEditingController titleTextController = TextEditingController();
   final TextEditingController noteTextController = TextEditingController();
+  final _db = DatabaseHelper.instance;
+  //  final FocusNode titlefn = FocusNode();
+  //  final FocusNode notefn = FocusNode();
 
   @override
   onClose() {
     titleTextController.dispose();
     noteTextController.dispose();
     super.onClose();
+  }
+
+  @override
+  onInit() {
+    super.onInit();
+    getAllNote();
   }
 
   addNote() {
@@ -23,7 +33,18 @@ class NoteController extends GetxController {
       createAt: DateTime.now().toIso8601String(),
     );
     log("called add note");
-    notes.add(note);
+    _db.insert(note.toJson());
+    getAllNote();
+  }
+
+  getAllNote() async {
+    final listNote = await _db.queryAllRows();
+    notes.value = listNote;
+  }
+
+  clear() {
+    titleTextController.clear();
+    noteTextController.clear();
   }
 
   removeNote({required int index}) {
---
+++ lib/database/database.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+
+import 'package:note/model/note.dart';
+import 'package:path/path.dart' as p;
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "note.db";
+  static final _databaseVersion = 1;
+
+  static final table = 'Notedb';
+
+  static final columnId = '_id';
+  static final columnTitle = 'title';
+  static final columnNote = 'note';
+  static final columnDate = 'createAt';
+
+  // make this a singleton class
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  // only have a single app-wide reference to the database
+  static Database? _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    // lazily instantiate the db the first time it is accessed
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  // this opens the database (and creates it if it doesn't exist)
+  _initDatabase() async {
+    String dbPath = await getDatabasesPath();
+    String dataPath = p.join(dbPath, _databaseName);
+    print(dataPath);
+
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = p.join(documentsDirectory.path, _databaseName);
+    print(path);
+    return await openDatabase(dataPath,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  // SQL code to create the database table
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table(
+	  $columnId INTEGER PRIMARY KEY
+  , $columnTitle  TEXT
+  ,$columnNote TEXT      
+  ,$columnDate  VARCHAR(20)
+);
+          ''');
+  }
+
+  // Helper methods
+
+  // Inserts a row in the database where each key in the Map is a column name
+  // and the value is the column value. The return value is the id of the
+  // inserted row.
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table, row,
+        conflictAlgorithm: ConflictAlgorithm.replace, nullColumnHack: "");
+  }
+
+  // All of the rows are returned as a list of maps, where each map is
+  // a key-value list of columns.
+  Future<List<Note>> queryAllRows() async {
+    Database db = await instance.database;
+    final data = await db.query(table);
+    final List<Note> noteList =
+        data.map<Note>((e) => Note.fromJson(e)).toList();
+    return noteList;
+  }
+
+  // Future<List<Map<String, dynamic>>> queryAllRowOrderByTabId() async {
+  //   Database db = await instance.database;
+  //   return await db.rawQuery('SELECT  * FROM ApiMan ORDER  BY "tabId" ASC ');
+  // }
+
+//TODO :ADD WORKSPACE support [AND workspace = "$input"]
+  // Future<List<Map<String, dynamic>>> queryAllApiRowOrderByTabId() async {
+  //   Database db = await instance.database;
+  //   return await db.rawQuery(
+  //       'SELECT  * FROM ApiMan WHERE "backend_type" = "http" ORDER  BY "tabId" ASC ');
+  // }
+
+  // Future<List<Map<String, dynamic>>> queryAllWsRowOrderByTabId() async {
+  //   Database db = await instance.database;
+  //   return await db.rawQuery(
+  //       'SELECT  * FROM ApiMan WHERE "backend_type" = "ws" ORDER  BY "tabId" ASC ');
+  // }
+
+  // All of the methods (insert, query, update, delete) can also be done using
+  // raw SQL commands. This method uses a raw query to give the row count.
+  Future<int?> queryRowCount() async {
+    Database db = await instance.database;
+    var i = await db.rawQuery('SELECT COUNT(*) FROM $table');
+    return Sqflite.firstIntValue(i);
+  }
+
+  // We are assuming here that the id column in the map is set. The other
+  // column values will be used to update the row.
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    int id = row[columnId];
+    return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future deleleAll() async {
+    Database db = await instance.database;
+    return await db.delete(table);
+  }
+
+  // Deletes the row specified by the id. The number of affected rows is
+  // returned. This should be 1 as long as the row exists.
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+}
